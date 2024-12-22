2a81bdcb874e2e5e00fdbf717ccf442f58b994ea
trungtruongpham <trungtruongpham@gmail.com>
2024-12-22 16:26:10 +0700

    Add next and previous buttons in end of page

--- src/app/[novel]/[chapter]/page.tsx
+++ src/app/[novel]/[chapter]/page.tsx
@@ -30,15 +30,20 @@ export default async function ChapterPage({
     const { title, content } = ChapterService.parseContent(html);
 
     return (
-      <div className="max-w-4xl mx-auto p-4">
+      <div className="max-w-4xl mx-auto p-4 flex flex-col gap-y-4">
         <ChapterNavigation
           prevIcon={<ChevronLeft className="w-5 h-5 mr-2" />}
           nextIcon={<ChevronRight className="w-5 h-5 ml-2" />}
           novel={novelInfo.novel}
           chapter={Number(chapterNumber)}
         />
-
         <ChapterContent title={title} content={content} />
+        <ChapterNavigation
+          prevIcon={<ChevronLeft className="w-5 h-5 mr-2" />}
+          nextIcon={<ChevronRight className="w-5 h-5 ml-2" />}
+          novel={novelInfo.novel}
+          chapter={Number(chapterNumber)}
+        />
       </div>
     );
   } catch (error) {
